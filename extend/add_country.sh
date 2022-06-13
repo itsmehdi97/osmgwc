b5d3380dc0b570699affe799687356e3d52db0e8
mehdi <kamani@ipreez.ir>
2022-06-13 17:54:01 +0430

    feat: install deps

--- extend/add_country.sh
+++ extend/add_country.sh
@@ -1,5 +1,8 @@
 source .env 
 
+which psql || sudo apt install -y postgresql-client
+which osm2pgsql || sudo apt install -y osm2pgsql
+
 psql --host=${HOST:-localhost} --port=${PORT:-5432} -U ${USER:-user} -d ${DB:-osm} -f sql/drop_unnecessary_tables.sql &&\
     osm2pgsql -H ${HOST:-localhost} -P ${PORT:-5432} -U ${USER:-user} -d ${DB:-osm} -r pbf <(wget -q -O - $1) &&\
     psql --host=${HOST:-localhost} --port=${PORT:-5432} -U ${USER:-user} -d ${DB:-osm} -f sql/categorized_insert.sql
